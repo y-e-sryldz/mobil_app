45836277efff8cc8e19a46446883862e1f8c7110
Merveziya <108355676+Merveziya@users.noreply.github.com>
2024-12-23 17:55:12 +0300

    battery status and wifi status completed using broadcast receiver

--- app/src/main/java/com/sari/firstapp/data/repository/UserRepository.kt
+++ app/src/main/java/com/sari/firstapp/data/repository/UserRepository.kt
@@ -1,7 +1,7 @@
 package com.sari.firstapp.data.repository
 
 import com.sari.firstapp.data.local.UserDao
-import com.sari.firstapp.data.remote.ApiService
+import com.sari.firstapp.data.service.ApiService
 
 class UserRepository(private val userDao: UserDao, private val apiService: ApiService) {
 
