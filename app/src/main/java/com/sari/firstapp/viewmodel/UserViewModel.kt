79109c503db175a692d817a7fa7f80ca1a92e17b
sencanmustafa <sencanmustafa54@gmail.com>
2024-12-25 12:29:05 +0300

    Implemented ViewModel

--- app/src/main/java/com/sari/firstapp/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/sari/firstapp/viewmodel/UserViewModel.kt
@@ -1,14 +1,6 @@
 package com.sari.firstapp.viewmodel
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.sari.firstapp.data.repository.UserRepository
-import kotlinx.coroutines.launch
+class UserViewModel(){
 
-class UserViewModel(private val repository: UserRepository) : ViewModel() {
-    fun fetchUsers() {
-        viewModelScope.launch {
 
-        }
-    }
 }
