8de414e82adc664f70ab5217086bda9f87690304
sari <emre242469@gmail.com>
2024-12-31 22:02:53 +0300

    usersSave commit

---
+++ app/src/main/java/com/sari/firstapp/feature/usersSave/usersSave.kt
@@ -0,0 +1,20 @@
+package com.sari.firstapp.feature.usersSave
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+@Composable
+fun UserDisplay() {
+
+    LaunchedEffect(Unit) {
+    }
+
+    // Kullanıcıları Göster
+    Column(modifier = Modifier.padding(16.dp)) {
+
+    }
+}
+
