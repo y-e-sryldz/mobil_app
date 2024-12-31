96fe97b78b8703fad2ad6bae593c6a74b27870f2
sari <emre242469@gmail.com>
2024-12-31 22:03:44 +0300

    usersSave

--- app/src/main/java/com/sari/firstapp/MainActivity.kt
+++ app/src/main/java/com/sari/firstapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.sari.firstapp
 
 import android.os.Bundle
+import android.util.Log
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,11 +21,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.*
 import androidx.compose.foundation.background
 import androidx.compose.ui.graphics.Color
-import android.util.Log
+import androidx.compose.foundation.lazy.items
+
 import androidx.compose.ui.Modifier
+
 import kotlinx.coroutines.launch
 import com.sari.firstapp.data.repository.UserRepository
 import com.sari.firstapp.feature.battery.BatteryLevelDisplay
@@ -51,22 +53,19 @@ fun MyApp() {
         Column {
             BatteryLevelDisplay(context = context)
             WifiStatusDisplay(context = context)
-            UserDisplay()
         }
     }
 }
 @Composable
-fun UserDisplay() {
-    val repository = UserRepository()
-    val scope = rememberCoroutineScope()
+fun UserDisplay(repository: UserRepository) {
 
-    // State for users and error messages
+    // Kullanıcı ve hata durumları için state
     var users by remember { mutableStateOf<List<ApiUserModel>>(emptyList()) }
     var errorMessage by remember { mutableStateOf<String?>(null) }
 
     // Fetch users
     LaunchedEffect(Unit) {
-        scope.launch {
+        try {
             val result = repository.getAllUsers()
             result.onSuccess { fetchedUsers ->
                 users = fetchedUsers
@@ -74,8 +73,12 @@ fun UserDisplay() {
                 errorMessage = error.message
                 Log.e("UserFetchError", "Error fetching users", error)
             }
+        } catch (e: Exception) {
+            errorMessage = e.message
+            Log.e("UserFetchError", "Exception occurred", e)
         }
     }
+
 
     // Show error message or loading indicator
     if (errorMessage != null) {
