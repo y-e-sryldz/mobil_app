3e814712a88c4df013785d378c3eebe38ee27cfa
Merveziya <108355676+Merveziya@users.noreply.github.com>
2025-01-02 12:54:43 +0300

    Location sensor integrated with map

--- app/src/main/java/com/sari/firstapp/MainActivity.kt
+++ app/src/main/java/com/sari/firstapp/MainActivity.kt
@@ -1,35 +1,39 @@
 package com.sari.firstapp
 
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.sari.firstapp.feature.battery.BatteryLevelDisplay
+import com.sari.firstapp.feature.map.MapScreen
 import com.sari.firstapp.feature.wifi.WifiStatusDisplay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             MyApp()
         }
     }
 }
+
 @Composable
 fun MyApp() {
     val context = LocalContext.current
 
     MaterialTheme {
-        Column {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
             BatteryLevelDisplay(context = context)
             WifiStatusDisplay(context = context)
-            Text(text = "Hello World!")
+            androidx.compose.foundation.layout.Spacer(modifier = Modifier.weight(1f))
+            MapScreen()
         }
     }
 }
